564b9983bef262b28ee20460e8dfeb79af4afc56
Joao Basanta <joao.basa.18@gmail.com>
2022-02-18 09:24:49 -0500

    Primera página - día 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import html2canvas from 'html2canvas'
 
 function App() {
+  const [linea1, setLinea1] = useState('')
+  const [linea2, setLinea2] = useState('')
+  const [imagen, setImagen] = useState('toystory')
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value)
+  }
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value)
+  }
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value)
+  }
+  const onClickExportar = function () {
+    html2canvas(document.querySelector('#meme')).then((canvas) => {
+      var img = canvas.toDataURL('image/jpg')
+      document.write('<img src="' + img + '"/>')
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="toystory">Toy Story</option>
+        <option value="nigga">WTF Nigga</option>
+        <option value="spider">Spiderverse</option>
+        <option value="stonks">Stonks</option>
+      </select>
+      <br />
+      <input
+        onChange={onChangeLinea1}
+        type="text"
+        placeholder="Texto Superior"
+      />
+      <br />
+      <input
+        onChange={onChangeLinea2}
+        type="text"
+        placeholder="Texto Inferior"
+      />
+      <br />
+      <button onClick={onClickExportar}>Exportar</button>
+      <br />
+      <div className="meme" id="meme">
+        <span>{linea1}</span>
+        <br />
+        <span>{linea2}</span>
+        <br />
+        <img src={'/images/' + imagen + '.jpg'} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
